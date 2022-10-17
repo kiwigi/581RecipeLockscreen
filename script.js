8db5ead801274e6a13eef63184ab35ffac47724c
Mar Ibarra <43395194+kiwigi@users.noreply.github.com>
2022-10-17 11:54:54 -0600

    testing animation

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie'),
+    renderer: "svg",
+    loop: true,
+    autoplay: false,
+    path: 'assets/data.json'
+})
